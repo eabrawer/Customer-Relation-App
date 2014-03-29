bb98c1495b3284ec52b9d7028c4d618761c50dca
eabrawer <ericbrawer@gmail.com>
2014-03-29 17:55:04 -0400

    In previous commit I added the ability to add contacts to the Contact array. In this commit contacts are now removable.

--- rolodex.rb
+++ rolodex.rb
@@ -1,4 +1,18 @@
 class Rolodex
 	@contacts = []
 	@id = 1000
+
+	def self.add_contact(name, email)
+		@id += 1
+		contact = Contact.new(@id, name, email)
+		@contacts << contact
+		@contacts[@contacts.length - 1]
+	end
+
+	def self.remove_contact(id)
+		@contacts.delete_if do |contact|
+			contact.id == id
+		end
+	end
+
 end
